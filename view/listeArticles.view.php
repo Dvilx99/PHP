f3f5e17f41fc51d58e8f756013b74d6324cf9ce6
TarJimmy <50171737+TarJimmy@users.noreply.github.com>
2019-11-11 11:09:42 +0100

    Correction bug

--- view/listeArticles.view.php
+++ view/listeArticles.view.php
@@ -10,19 +10,12 @@
 
     <body>
       <?php ComposantsVue::creationHeader(); ?>
-      <nav>
-    <!-- Bouton de retour au début de la liste -->
-    <a href="../controler/afficherListeArticles.ctrl.php?categorie=0"><img src="../view/design/home.jpeg"/></a>
-    <!-- Bouton de retour à la page précédente -->
-    <a href="../controler/afficherListeArticles.ctrl.php?ref=<?=$prevRef?>"><img src="../view/design/flecheG.png"/> </a>
-    <!-- Bouton pour passer à la page suivante -->
-    <a href="../controler/afficherListeArticles.ctrl.php?ref=<?=$nextRef?>"><img src="../view/design/flecheD.png"/></a>
-  </nav>
+
       <!-- >Choix du nombre d'article -->
       <form class="choixNbArticle" action="../controler/afficherListeArticles.ctrl.php?categorie=<?=$catCourante?>" method="POST">
        <fieldset>
         <label for="name">Nombre d'article présent sur la page:</label>
-        <input type="number" id="nbArticle" name="nbArticle" value="<?=$_SESSION['nbArticle']?>" size = "2">
+        <input type="number" id="nbArticle" name="nbArticle" value="<?=$nbArticle?>" size = "2">
          <input type="submit" value=" Valider " size = "10">
        </fieldset>
        <!--Affichage des articles-->
