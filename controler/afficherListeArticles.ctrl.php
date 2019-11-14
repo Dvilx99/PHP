bb26341e4fe3a82f900ff46e3540f7c07f8db198
TarJimmy <50171737+TarJimmy@users.noreply.github.com>
2019-11-14 18:37:05 +0100

    ajoutPanier->modifPanier
    suppression code inutile + fonctionnalite suppression article au panier

--- controler/afficherListeArticles.ctrl.php
+++ controler/afficherListeArticles.ctrl.php
@@ -9,10 +9,10 @@
     //Declaration Variable + valeur par default
     $dao = new DAO();
     $view = new View();
+    $config = parse_ini_file('../config/config.ini');
     //Si il y une session je continue
     //Sinon j'envoie à la connexion
     if (isset($_SESSION['isConnected'])){
-      $config = parse_ini_file('../config/config.ini');
       $liste = array();
       //Charge le nombre d'article que l'utilisateur veut afficher ou met 5 par default,
       //Ou ne fais rien car il n'y a pas d'action sur nbArticle
@@ -21,13 +21,6 @@
       }
       //Charge la categorie courante que l'utilisateur veut afficher ou met 0 par default,
       //Ou ne fais rien car il n'y a pas d'action sur Categorie
-      // if (isset($_GET['categorie'])){
-      //   $_SESSION['categorie'] = $_GET['categorie'];
-      // }
-      // else if (!isset($_SESSION['categorie'])){
-      //   $_SESSION['categorie'] = 0;
-      // }
-      // Tout ce code =
       $_SESSION['categorie'] = $_GET['categorie'] ?? 0;
 
       //Si la categorie existe prepare le chargement pour celle ci,affichie l'accueil sinon
@@ -37,30 +30,6 @@
       else {
         $liste = $dao->firstN($_SESSION['nbArticle']);
       }
-      // Note la référence du premier et dernier article affiché
-
-      //Je commente ca marche pas mais du coup quand on choisit une catégorie le nb articles marche plus.
-      /*if (sizeof($liste)>0) {
-        $firstRef = $liste[0]->getRef();
-        $lastRef = end($liste)->getRef();
-
-        // Calcule la référence qui suit le dernier article
-        $nextRef = $dao->next($lastRef);
-        // Si c'est la fin: reste sur le même article
-        if ($nextRef == -1) {
-          $nextRef = $firstRef;
-        }
-        // Passe le résultat à la vue
-        $view->assign('nextRef',$nextRef);
-        // Calcule la référence qui précède du nombre d'article que l'utilisateur veut afficher l'article courant
-        $prevRef = $dao->prevN($firstRef,$_SESSION['nbArticle']);
-        // Si c'est la fin: reste sur le même article
-        if ($prevRef == -1) {
-          $prevRef = $firstRef;
-        }
-        // Passe le résultat à la vue
-        $view->assign('prevRef',$prevRef);
-      }*/
 
       //Donne le chemin pour récupérer les images a la vue
       $view->assign('chemin',$config['images_path']);
